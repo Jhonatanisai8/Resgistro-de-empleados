17921d2f5ced30522375a828392605bcb29f5fd5
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-10 10:01:26 -0500

    Programacion del formulario y llamando en la clase main

---
+++ src/sistema/registro/empleados/ucv/SistemaRegistroEmpleadosUcv.java
@@ -0,0 +1,11 @@
+
+package sistema.registro.empleados.ucv;
+
+public class SistemaRegistroEmpleadosUcv {
+
+    public static void main(String[] args) {
+       frmRegistro p = new frmRegistro();
+       p.setVisible(true);
+    }
+    
+}
